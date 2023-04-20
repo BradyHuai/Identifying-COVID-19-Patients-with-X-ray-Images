b49f05fc6d6e9821a841a388bd9090f5c44d34c3
KevinHu1231 <56094961+KevinHu1231@users.noreply.github.com>
2023-04-19 23:16:00 -0400

    Added baseline class

--- train.py
+++ train.py
@@ -8,7 +8,31 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from preprocess import correction, dataload
 
-
+class Baseline(Module):
+	def __init__(self, numChannels, classes):
+		self.conv1 = Conv2d(in_channels=numChannels, out_channels=10,kernel_size=(3,3))
+		self.relu1 = ReLU()
+		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+		self.conv2 = Conv2d(in_channels=10, out_channels=50,kernel_size=(3,3))
+		self.relu2 = ReLU()
+		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+		self.fc1 = Linear(in_features=29160, out_features=64)
+        self.relu3 = ReLU()
+        self.fc2 = Linear(in_features=64, out_features=3)
+  
+  def forward(self,x):
+		x = self.conv1(x)
+		x = self.relu1(x)
+		x = self.maxpool1(x)
+		x = self.conv2(x)
+		x = self.relu2(x)
+		x = self.maxpool2(x)
+		x = flatten(x, 1)
+		x = self.fc1(x)
+		x = self.relu3(x)
+		x = self.fc2(x)
+		return x
+    
 def make_classifier(in_features, num_classes):
     """
     Creates a linear classifier with dropout and ReLU activation.
